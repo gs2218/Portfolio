e02072f429ac2f8e310c5c7033a8329ddd717748
Guy Solan <thetismedical@gmail.com>
2021-05-01 14:24:04 +0200

    burger-mobile

--- app.js
+++ app.js
@@ -84,28 +84,36 @@ function cursor(e) {
 }
 function activeCursor(e) {
   const item = e.target;
-  if (item.id === "logo" || item.classList.contains("burger") ||
-    item.classList.contains("footer-text")) {
-    mouse.classList.add("nav-active");
-  } else {
-    mouse.classList.remove("nav-active");
-  }
-  if (item.classList.contains("explore") || item.classList.contains("arrow-button")) {
-    mouse.classList.add("explore-active");
-    gsap.to(".title-swipe", 1, { y: "0%" });
-    mouseTxt.innerText = "Tap";
-  } else {
-    mouse.classList.remove("explore-active");
-    mouseTxt.innerText = "";
-    gsap.to(".title-swipe", 1, { y: "100%" });
+  if (window.innerWidth > 1024) {
+    if (item.id === "logo" || item.classList.contains("burger") ||
+      item.classList.contains("footer-text")) {
+      mouse.classList.add("nav-active");
+    } else {
+      mouse.classList.remove("nav-active");
+    }
+    if (item.classList.contains("explore") || item.classList.contains("arrow-button")) {
+      mouse.classList.add("explore-active");
+      gsap.to(".title-swipe", 1, { y: "0%" });
+      mouseTxt.innerText = "Tap";
+    } else {
+      mouse.classList.remove("explore-active");
+      mouseTxt.innerText = "";
+      gsap.to(".title-swipe", 1, { y: "100%" });
+    }
   }
 }
 function navToggle(e) {
   if (!e.target.classList.contains("active")) {
     e.target.classList.add("active");
     gsap.to(".line", 0.5, { stroke: "white" });
-    gsap.to(".line1", 0.5, { rotate: "45", y: 12, stroke: "white" });
-    gsap.to(".line2", 0.5, { rotate: "-45", x: 1, y: -7, stroke: "white" });
+    if (window.innerWidth < 1024) {
+      gsap.to(".line1", 0.5, { rotate: "45", y: 10, stroke: "white" });
+      gsap.to(".line2", 0.5, { rotate: "-45", x: -4, y: -4, stroke: "white" });
+    }
+    else {
+      gsap.to(".line1", 0.5, { rotate: "45", y: 12, stroke: "white" });
+      gsap.to(".line2", 0.5, { rotate: "-45", x: 1, y: -7, stroke: "white" });
+    }
     gsap.to("#logo", 1, { color: "white" });
     gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
     document.body.classList.add("hide");
